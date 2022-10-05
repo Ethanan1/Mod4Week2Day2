e40cb2a26d65a69fc5d286d6f9d56f7a1e31acfd
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2022-10-04 19:34:10 -0700

    dbdiagram

---
+++ practice-for-week-10-sql-database-tables-visualizer/practice.js
@@ -0,0 +1,15 @@
+// table colors {
+//     id integer PK
+//     name text
+//   }
+
+//   table music {
+//     id integer PK
+//     name text
+//     type_of_work text
+//     instrumental boolean
+//     composition_year integer
+//     composer text
+//     sample blob
+//   }
+//USE ON DBDIAGRAM, NOT FOR JS
